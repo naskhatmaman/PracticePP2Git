f1fe50f74a2916ac82cde32ed4a7804db67cc924
Maman Naskhat <naskhatmaman@gmail.com>
2026-01-28 00:27:58 +0500

    variables added

---
+++ variables.py
@@ -0,0 +1,18 @@
+x = 5
+y = "John"
+z = 10.5
+
+a, b, c = 1, 2, "three"
+
+count = 10
+count = count + 5
+count += 5
+
+val_1 = 100
+val_2 = 200
+val_1, val_2 = val_2, val_1
+
+print(x, y, z)
+print(a, b, c)
+print(count)
+print(val_1, val_2)
