8c3dd0308d26fdc07c0f218b7f39de1aa7c2a97d
Maman Naskhat <naskhatmaman@gmail.com>
2026-01-28 01:27:43 +0500

    data_type added

---
+++ Project1/data_types.py
@@ -0,0 +1,21 @@
+integer_val = 42
+float_val = 3.14
+string_val = ""
+boolean_val = True
+
+list_val = [1, 2, 2, "test"]
+tuple_val = (1, 2, 3)
+set_val = {1, 2, 3, 4, 5}
+dict_val = {
+    "name": "f",
+    "age": 25,
+}
+
+print(type(integer_val))
+print(type(float_val))
+print(type(string_val))
+print(type(boolean_val))
+print(type(list_val))
+print(type(tuple_val))
+print(type(set_val))
+print(type(dict_val))
